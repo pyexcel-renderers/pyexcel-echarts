f454629ee49c4492dcfe798121efa911b497b77b
chfw <wangc_2011@hotmail.com>
2017-07-16 22:23:53 +0100

    adding timeseries chart

--- pyexcel_echarts/options.py
+++ pyexcel_echarts/options.py
@@ -1,6 +1,7 @@
 import json
 from collections import defaultdict
 
+from datetime import date, datetime
 from lml.plugin import PluginInfo, PluginManager
 
 
@@ -8,29 +9,36 @@ DEFAULTS = dict(
     title={},
     tooltip={},
     legend=defaultdict(list),
-    xAxis={},
-    yAxis={},
     series=[]
 )
 
 
+class DateTimeEncoder(json.JSONEncoder):
+    def default(self, obj):
+        if isinstance(obj, date):
+            date_string = obj.strftime('%Y/%m/%d')
+            return date_string
+        if isinstance(obj, datetime):
+            datetime_string = obj.strftime("%Y/%m/%d %H:%M:%S")
+            return datetime_string
+        return json.JSONEncoder.default(self, obj)
+
+
+def _dumps(data):
+    return json.dumps(data, cls=DateTimeEncoder)
+
+
 class EChartsOption(object):
     def __init__(self):
-        self.__config = {}
-        self.__config.update(DEFAULTS)
+        self._config = {}
+        self._config.update(DEFAULTS)
 
     def set_title(self, title):
-        self.__config['title']['text'] = title
-
-    def set_axis_x(self, data):
-        self.__config['xAxis']['data'] = data
-
-    def set_axis_y(self, data):
-        self.__config['yAxis']['data'] = data
+        self._config['title']['text'] = title
 
     def add_chart(self, name, chart_type, data):
-        self.__config['legend']['data'].append(name)
-        self.__config['series'].append(
+        self._config['legend']['data'].append(name)
+        self._config['series'].append(
             dict(
                 name=name,
                 type=chart_type,
@@ -41,7 +49,7 @@ class EChartsOption(object):
         raise NotImplementedError("")
 
     def to_json(self):
-        return json.dumps(self.__config)
+        return _dumps(self._config)
 
 
 @PluginInfo(
@@ -49,6 +57,11 @@ class EChartsOption(object):
     tags=['bar']
     )
 class BarOption(EChartsOption):
+
+    def __init__(self):
+        super(BarOption, self).__init__()
+        self._config['xAxis'] = {}
+        self._config['yAxis'] = {}
 
     def configure(self, sheet,
                   label_x_in_column=0, label_y_in_row=0, **keywords):
@@ -58,6 +71,54 @@ class BarOption(EChartsOption):
         self.set_axis_x(sheet.colnames)
         for rowname in sheet.rownames:
             self.add_chart(rowname, 'bar', sheet.row[rowname])
+
+    def set_axis_x(self, data):
+        self._config['xAxis']['data'] = data
+
+    def set_axis_y(self, data):
+        self._config['yAxis']['data'] = data
+
+
+@PluginInfo(
+    'echarts',
+    tags=['timeseries']
+)
+class TimeSeriesOption(EChartsOption):
+    def __init__(self):
+        super(TimeSeriesOption, self).__init__()
+        self._config['xAxis'] = []
+        self._config['yAxis'] = []
+
+    def configure(self, sheet,
+                  timeseries_in_column=0, value_in_column=1, **keywords):
+        self.set_title(sheet.name)
+        self._config['xAxis'].append(dict(
+            type='category',
+            boundayGap=False,
+            axisLine=dict(onZero=True),
+            data=sheet.column[timeseries_in_column],
+            position='top'
+        ))
+        self._config['yAxis'].append(dict(
+            name='NO2(xx)',
+            type='value',
+            max=500
+        ))
+        self._config['tooltip'] = dict(
+            trigger='axis',
+            axisPointer=dict(animation=False)
+        )
+        self._config['axisPointer'] = dict(link=dict(xAxisIndex='all'))
+        self._config['legend'] = dict(
+            data=['NO2']
+        )
+        self._config['series'].append(dict(
+            name='NO2',
+            type='line',
+            symbolSize=8,
+            hoverAnimation=False,
+            data=sheet.column[value_in_column]
+        ))
 
 
 class EChartsOptionManager(PluginManager):
